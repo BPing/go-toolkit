ff2102454c7f699fd14a428a363d07bc383b6450
cbping <452775680@qq.com>
2016-08-19 14:29:14 +0800

    go fmt

--- resp/mResp.go
+++ resp/mResp.go
@@ -14,32 +14,31 @@ type RespInfo struct {
 //@Description
 const (
 	//commom
-	RESP_OK = 10000
-	RESP_ERR = 10001
-	RESP_PARAM_ERR = 10002
-	RESP_TOKEN_ERR = 10003
-	RESP_NO_ACCESS = 10004
-	RESP_APP_NOT_ON = 10005
+	RESP_OK          = 10000
+	RESP_ERR         = 10001
+	RESP_PARAM_ERR   = 10002
+	RESP_TOKEN_ERR   = 10003
+	RESP_NO_ACCESS   = 10004
+	RESP_APP_NOT_ON  = 10005
 	RESP_UNKNOWN_ERR = 10006
 )
 
 //响应码描述
 var respMsg map[int]string = map[int]string{
-	RESP_OK:        "操作成功",
-	RESP_ERR:       "操作失败",
-	RESP_PARAM_ERR: "参数错误",
-	RESP_TOKEN_ERR: "签名认证错误",
-	RESP_NO_ACCESS: "对不起，您没有此操作权限",
-	RESP_APP_NOT_ON: "暂时未提供服务",
+	RESP_OK:          "操作成功",
+	RESP_ERR:         "操作失败",
+	RESP_PARAM_ERR:   "参数错误",
+	RESP_TOKEN_ERR:   "签名认证错误",
+	RESP_NO_ACCESS:   "对不起，您没有此操作权限",
+	RESP_APP_NOT_ON:  "暂时未提供服务",
 	RESP_UNKNOWN_ERR: "未知错误",
 
-
-	http.StatusBadRequest:        "参数有误（Bad Request）",
+	http.StatusBadRequest:          "参数有误（Bad Request）",
 	http.StatusUnauthorized:        "用户认证不成功",
 	http.StatusForbidden:           "拒绝访问",
 	http.StatusNotFound:            "资源不存在",
 	http.StatusMethodNotAllowed:    "此方法未允许访问",
-	http.StatusRequestTimeout:    "请求远程服务器超时",
+	http.StatusRequestTimeout:      "请求远程服务器超时",
 	http.StatusInternalServerError: "服务器内部错误",
 	http.StatusServiceUnavailable:  "服务不可用",
 }
